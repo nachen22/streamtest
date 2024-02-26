0a0c722bc321f852f0f07c2e8ce7f30acae403d7
nachen22 <112440917+nachen22@users.noreply.github.com>
2024-02-26 14:10:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
 
 st.write('Hello world!')
 
