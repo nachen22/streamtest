463a7f097e97b4d5dde7c659ba159368192484a7
nachen22 <112440917+nachen22@users.noreply.github.com>
2024-02-28 20:48:32 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,16 +23,6 @@ st.latex(r'''
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
 
-st.write('Contents of the `.streamlit/config.toml` file of this app')
+st.write(st.secrets['message test test'])
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
 
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
