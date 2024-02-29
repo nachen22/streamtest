396c2d6e23e13e13a717a4f1a693c71819b48a7e
nachen22 <112440917+nachen22@users.noreply.github.com>
2024-02-28 20:18:04 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,9 @@ st.subheader('Slider')
 
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
+
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
